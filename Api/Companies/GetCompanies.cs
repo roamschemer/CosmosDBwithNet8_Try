688f3f9365739fd7c8a169fb8423e7706ad656d6
roamschemer <roamschemer@gmail.com>
2024-06-29 13:21:14 +0900

    CosmosDBヘの読み書き成立。絞りこみ以外。

---
+++ Api/Companies/GetCompanies.cs
@@ -0,0 +1,51 @@
+using Data;
+using Google.Protobuf.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Companies
+{
+	public class GetCompanies
+	{
+		private readonly ILogger<GetCompanies> _logger;
+
+		public GetCompanies(ILogger<GetCompanies> logger) {
+			_logger = logger;
+		}
+
+		[Function(nameof(GetCompanies))]
+		public async Task<IActionResult> Run(
+			[HttpTrigger(AuthorizationLevel.Function, "get", Route = "companies")] HttpRequest req,
+			[CosmosDBInput(Connection = "CosmosDBConnection")] CosmosClient client) {
+			_logger.LogInformation("C# HTTP trigger function processed a request.");
+
+			var name = req.Query["name"].ToString();
+
+			var databaseName = Environment.GetEnvironmentVariable("CosmosDb");
+
+			var container = client.GetContainer(databaseName, "companies");
+
+			IQueryable<Company> queryable = container.GetItemLinqQueryable<Company>()
+				.Where(c => string.IsNullOrEmpty(name) || c.Name.Contains(name))
+				.OrderByDescending(c => c.CreatedAt);
+
+			var iterator = queryable.ToFeedIterator();
+
+			var companies = new List<Company>();
+			while (iterator.HasMoreResults) {
+				var response = await iterator.ReadNextAsync();
+				companies.AddRange(response);
+			}
+
+			return new OkObjectResult(companies);
+		}
+	}
+}
---
+++ Data/Company.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Data
+{
+	public class Company()
+	{
+		public enum CategoryDatas
+		{
+			Admin,
+			User,
+			PowerUser,
+			Customer,
+		}
+
+		[JsonPropertyName("id")]
+		public string Id { get; set; }
+
+		[JsonPropertyName("category")]
+		[JsonConverter(typeof(JsonStringEnumConverter))]
+		public CategoryDatas Category { get; set; } = CategoryDatas.User;
+
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("created_at")]
+		public DateTime CreatedAt { get; set; }
+	}
+}
