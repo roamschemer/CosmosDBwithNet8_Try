9eeca1bff9233ce66ce8ff2daa675f54021d4932
roamschemer <roamschemer@gmail.com>
2024-07-08 05:37:37 +0900

    テストの環境設定ファイルを読み込み

---
+++ Test/Api/Repositories/CompanyRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using Api.Validators.Companies;
+
+namespace Test.Api.Repositories
+{
+	[TestClass]
+	public class CompanyRepositoryTest
+	{
+		public TestContext TestContext { get; set; }
+		private IPostCompanyValidator _validator;
+
+		[TestInitialize]
+		public void Setup() {
+			var connectionString = TestContext.Properties["CosmosDBConnection"];
+			var databaseId = TestContext.Properties["CosmosDb"];
+		}
+
+		[TestMethod]
+		public void Validate_正常() {
+		}
+
+	}
+}
