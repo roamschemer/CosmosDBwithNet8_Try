b9497ff72e5cbaa78dbccad949a244d37fe30f79
roamschemer <roamschemer@gmail.com>
2024-07-11 06:50:11 +0900

    get Api のテストを作成

---
+++ Test/Api/Apis/getCompaniesTest.cs
@@ -0,0 +1,103 @@
+﻿using Api.Controllers.Companies;
+using Api.Repositories;
+using Api.Utils;
+using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using Newtonsoft.Json;
+using Test.Factories;
+
+namespace Test.Api.Apis
+{
+	[TestClass]
+	public class GetCompaniesTest
+	{
+		public TestContext TestContext { get; set; }
+		private Container _companyContainer;
+		private Random _random = new();
+		private GetCompanies _getCompanies;
+
+		[TestInitialize]
+		public async Task Setup() {
+			var dbInitializer = new CosmosDbInitializer(TestContext.Properties["CosmosDBConnection"]?.ToString(), TestContext.Properties["CosmosDb"]?.ToString());
+			_companyContainer = await dbInitializer.GetContainerAsync("companies", "/" + "category", isCleanUp: true);
+			_getCompanies = new GetCompanies(
+				new Mock<ILogger<GetCompanies>>().Object,
+				new CompanyRepository(new Mock<ILogger<ICompanyRepository>>().Object, _companyContainer)
+			);
+		}
+
+		[TestMethod]
+		public async Task Run_クエリパラメータなし() {
+			var targetCompanies = CompanyFactory.Generate(10);
+			await Task.WhenAll(targetCompanies.Select(company => _companyContainer.CreateItemAsync(company, new PartitionKey((int)company.Category))));
+
+			var queryString = new Dictionary<string, StringValues> { };
+			var httpRequest = new DefaultHttpContext().Request;
+			httpRequest.Query = new QueryCollection(queryString);
+
+			var result = await _getCompanies.Run(httpRequest);
+
+			Assert.IsNotNull(result);
+			var okResult = result as OkObjectResult;
+			Assert.IsNotNull(okResult);
+			var getCompanies = okResult.Value as List<Company>;
+			Assert.AreEqual(targetCompanies.Count, getCompanies.Count, "取得数一致");
+			foreach (var (testCompany, index) in targetCompanies.OrderByDescending(x => x.CreatedAt).Select((x, i) => (x, i))) {
+				var expectedCompanyJson = JsonConvert.SerializeObject(testCompany);
+				var actualCompanyJson = JsonConvert.SerializeObject(getCompanies[index]);
+				Assert.AreEqual(expectedCompanyJson, actualCompanyJson, "内容一致");
+			}
+		}
+
+		[TestMethod]
+		public async Task Run_クエリパラメータ_name() {
+			var targetCompanies = CompanyFactory.Generate(10);
+			await Task.WhenAll(targetCompanies.Select(company => _companyContainer.CreateItemAsync(company, new PartitionKey((int)company.Category))));
+
+			var selectCompanyName = targetCompanies.OrderBy(x => _random.Next()).FirstOrDefault().Name.Substring(1, 3);
+			var queryString = new Dictionary<string, StringValues> {
+				{ "name", selectCompanyName }
+			};
+
+			var httpRequest = new DefaultHttpContext().Request;
+			httpRequest.Query = new QueryCollection(queryString);
+
+			var result = await _getCompanies.Run(httpRequest);
+
+			Assert.IsNotNull(result);
+			var okResult = result as OkObjectResult;
+			Assert.IsNotNull(okResult);
+			var getCompanies = okResult.Value as List<Company>;
+			Assert.AreEqual(targetCompanies.Where(x => x.Name.Contains(selectCompanyName)).Count(), getCompanies.Count, "全取得数一致");
+			Assert.IsTrue(getCompanies.All(x => x.Name.Contains(selectCompanyName)), "Name は部分一致");
+		}
+
+		[TestMethod]
+		public async Task Run_クエリパラメータ_category() {
+			var targetCompanies = CompanyFactory.Generate(10);
+			await Task.WhenAll(targetCompanies.Select(company => _companyContainer.CreateItemAsync(company, new PartitionKey((int)company.Category))));
+
+			var selectCompanyCategory = targetCompanies.OrderBy(x => _random.Next()).FirstOrDefault().Category;
+			var queryString = new Dictionary<string, StringValues> {
+				{ "category", ((int)selectCompanyCategory).ToString() }
+			};
+
+			var httpRequest = new DefaultHttpContext().Request;
+			httpRequest.Query = new QueryCollection(queryString);
+
+			var result = await _getCompanies.Run(httpRequest);
+
+			Assert.IsNotNull(result);
+			var okResult = result as OkObjectResult;
+			Assert.IsNotNull(okResult);
+			var getCompanies = okResult.Value as List<Company>;
+			Assert.AreEqual(targetCompanies.Where(x => x.Category == selectCompanyCategory).Count(), getCompanies.Count, "全取得数一致");
+			Assert.IsTrue(getCompanies.All(x => x.Category == selectCompanyCategory), "category は完全一致");
+		}
+	}
+}
--- Test/Api/Repositories/CompanyRepositoryTest.cs
+++ Test/Api/Repositories/CompanyRepositoryTest.cs
@@ -5,6 +5,7 @@ using Data;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Newtonsoft.Json;
 using Test.Factories;
 
 namespace Test.Api.Repositories
@@ -29,22 +30,28 @@ namespace Test.Api.Repositories
 
 		[TestMethod]
 		public async Task SelectConditionsAsync() {
-			//ダミーデータの差し込み
-			var testCompanies = CompanyFactory.Generate(10);
-			await Task.WhenAll(testCompanies.Select(company => _container.CreateItemAsync(company, new PartitionKey((int)company.Category))));
+			// ターゲットの差し込み
+			var targetCompanies = CompanyFactory.Generate(10);
+			await Task.WhenAll(targetCompanies.Select(company => _container.CreateItemAsync(company, new PartitionKey((int)company.Category))));
 
 			// 実行
-			var companies = await _repository.SelectConditionsAsync(new() { });
-			Assert.AreEqual(testCompanies.Count, companies.Count, "全取得");
+			var getCompanies = await _repository.SelectConditionsAsync(new() { });
+			Assert.AreEqual(targetCompanies.Count, getCompanies.Count, "全取得数一致");
+			foreach (var (testCompany, index) in targetCompanies.OrderByDescending(x => x.CreatedAt).Select((x, i) => (x, i))) {
+				var expectedCompanyJson = JsonConvert.SerializeObject(testCompany);
+				var actualCompanyJson = JsonConvert.SerializeObject(getCompanies[index]);
+				Assert.AreEqual(expectedCompanyJson, actualCompanyJson, "内容一致");
+			}
 
-			var targetCompany = testCompanies.OrderBy(x => _random.Next()).FirstOrDefault();
-			var selectName = targetCompany.Name.Substring(1, 3);
-			companies = await _repository.SelectConditionsAsync(new() { { "name", selectName } });
-			Assert.IsTrue(companies.All(x => targetCompany.Name.Contains(selectName)), "Name は部分一致");
+			var selectCompanyName = targetCompanies.OrderBy(x => _random.Next()).FirstOrDefault().Name.Substring(1, 3);
+			getCompanies = await _repository.SelectConditionsAsync(new() { { "name", selectCompanyName } });
+			Assert.AreEqual(targetCompanies.Where(x => x.Name.Contains(selectCompanyName)).Count(), getCompanies.Count, "全取得数一致");
+			Assert.IsTrue(getCompanies.All(x => x.Name.Contains(selectCompanyName)), "Name は部分一致");
 
-			var selectCategory = targetCompany.Category;
-			companies = await _repository.SelectConditionsAsync(new() { { "category", selectCategory.ToString() } });
-			Assert.IsTrue(companies.All(x => targetCompany.Name.Contains(selectName)), "category は完全一致");
+			var selectCompanyCategory = targetCompanies.OrderBy(x => _random.Next()).FirstOrDefault().Category;
+			getCompanies = await _repository.SelectConditionsAsync(new() { { "category", ((int)selectCompanyCategory).ToString() } });
+			Assert.AreEqual(targetCompanies.Where(x => x.Category == selectCompanyCategory).Count(), getCompanies.Count, "全取得数一致");
+			Assert.IsTrue(getCompanies.All(x => x.Category == selectCompanyCategory), "category は完全一致");
 		}
 
 		[TestMethod]
